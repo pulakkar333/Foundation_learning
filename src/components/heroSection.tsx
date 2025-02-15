a6f33a031eca432ae0ff21d94aa51972d988fa21
pulakkar333 <pulakkaar@gmail.com>
2025-02-15 11:17:51 +0600

    responsive

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -41,19 +41,22 @@ const classes = [
 
 export default function HeroSection() {
   return (
-    <div className="bg-[#dff2f5] w-full flex flex-col px-4 sm:px-6 lg:px-20">
-      <div className="py-6">
+    <div className="bg-[#dff2f5] w-full flex flex-col">
+      <div
+        className="sm:py-6 sm:px-6 md:px-10 mt-[30px]"
+        style={{ paddingLeft: "80px", paddingRight: "375px" }}
+      >
         <div className="flex flex-col md:flex-row md:justify-between space-y-6 md:space-y-0">
-          <h1 className="text-2xl sm:text-3xl md:text-4xl font-light text-gray-900">
+          <h1 className="text-3xl sm:text-4xl font-light text-gray-900 mt-4">
             Hello <em className="italic">Emma</em>,<br /> Welcome Back!
           </h1>
 
           <div className="w-full md:w-1/3 border-l border-gray-300 pt-4 md:pl-6">
             <div className="flex justify-between border-b border-gray-300 pb-4">
-              <button className="px-4 py-2 bg-white text-[#2F5852] text-sm font-medium rounded-full shadow-md">
+              <button className="px-5 py-2 bg-white text-[#2F5852] text-sm font-medium rounded-full shadow-md">
                 Classes
               </button>
-              <button className="px-3 py-2 text-sm text-[#2F5852] rounded-full hover:bg-blue-100 transition">
+              <button className="px-4 py-2 text-sm text-[#2F5852] rounded-full hover:bg-blue-100 transition">
                 + Add Class
               </button>
             </div>
@@ -68,7 +71,7 @@ export default function HeroSection() {
 
           <div className="w-full md:w-1/3 border-t md:border-t-0 md:border-l border-gray-300 pt-4 md:pl-6">
             <div>
-              <button className="px-4 py-2 bg-white text-[#2F5852] text-sm font-medium rounded-full shadow-md">
+              <button className="px-5 py-2 bg-[#FFFFFF] text-[#2F5852] text-sm font-medium rounded-full shadow-md">
                 Overview
               </button>
             </div>
@@ -78,10 +81,10 @@ export default function HeroSection() {
             </ul>
           </div>
         </div>
-        <hr className="mt-6 border-t border-gray-300" />
+        <hr className="mt-8 border-t border-gray-300" />
       </div>
 
-      <div className="pb-6">
+      <div style={{ paddingLeft: "80px", paddingRight: "50px" }}>
         <Slider title="Recent Classes" data={classes} />
       </div>
     </div>
