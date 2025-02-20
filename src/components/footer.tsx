7c55de54fba6ae37afd140620fe59e25fd427066
pulakkar333 <pulakkaar@gmail.com>
2025-02-20 19:34:20 +0600

    redesign

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,16 +5,40 @@ export default function Footer() {
     <footer className="bg-[#F5FBFC] text-gray-800 p-10 mt-6">
       <div className="container mx-auto lg:grid lg:grid-cols-4 lg:gap-4 text-[12px] ">
         <div className="border-r pr-4">
-          <h3 className="font-bold">SUBJECTS</h3>
+          <h3
+            className="
+    font-bold 
+    text-left 
+    uppercase 
+    text-[14px] 
+    font-dm-sans 
+    text-[rgba(47,88,82,0.60)]
+    overflow-hidden 
+    text-ellipsis 
+    whitespace-nowrap
+    mb-[7px]
+  "
+            style={{
+              display: "-webkit-box",
+              transform: "rotate(-0.092deg)",
+              WebkitBoxOrient: "vertical",
+              WebkitLineClamp: 1,
+              lineHeight: "normal",
+            }}
+          >
+            SUBJECTS
+          </h3>
+
           <div className="grid grid-cols-2 gap-2">
-            <ul>
+            <ul className="w-[200px] text-[#2F5852] text-[16px] font-dm-sans font-[16px] leading-normal">
               <li>Math</li>
               <li>Science</li>
               <li>English Language</li>
               <li>Homeschool Support</li>
               <li>Study Skills</li>
             </ul>
-            <ul>
+
+            <ul className="w-[200px] text-[#2F5852] text-[16px] font-dm-sans font-[16px] leading-normal">
               <li>Algebra</li>
               <li>Geometry</li>
               <li>Precalculus</li>
@@ -24,8 +48,30 @@ export default function Footer() {
           </div>
         </div>
         <div className="border-r px-4">
-          <h3 className="font-bold">TEST PREP</h3>
-          <ul>
+          <h3
+            className="
+    font-bold 
+    text-left 
+    uppercase 
+    text-[14px] 
+    font-dm-sans 
+    text-[rgba(47,88,82,0.60)]
+    overflow-hidden 
+    text-ellipsis 
+    whitespace-nowrap
+    mb-[7px]
+  "
+            style={{
+              display: "-webkit-box",
+              transform: "rotate(-0.092deg)",
+              WebkitBoxOrient: "vertical",
+              WebkitLineClamp: 1,
+              lineHeight: "normal",
+            }}
+          >
+            TEST PREP
+          </h3>
+          <ul className="w-[200px] text-[#2F5852] text-[16px] font-dm-sans font-[16px] leading-normal">
             <li>SAT</li>
             <li>DSAT</li>
             <li>ACT</li>
@@ -35,15 +81,59 @@ export default function Footer() {
           </ul>
         </div>
         <div>
-          <h3 className="font-bold mb-2">COLLEGE READINESS</h3>
-          <ul>
+          <h3
+            className="
+    font-bold 
+    text-left 
+    uppercase 
+    text-[14px] 
+    font-dm-sans 
+    text-[rgba(47,88,82,0.60)]
+    overflow-hidden 
+    text-ellipsis 
+    whitespace-nowrap
+    mb-[7px]
+  "
+            style={{
+              display: "-webkit-box",
+              transform: "rotate(-0.092deg)",
+              WebkitBoxOrient: "vertical",
+              WebkitLineClamp: 1,
+              lineHeight: "normal",
+            }}
+          >
+            COLLEGE READINESS
+          </h3>
+          <ul className="w-[200px] text-[#2F5852] text-[16px] font-dm-sans font-[16px] leading-normal">
             <li>AP</li>
             <li>IB</li>
           </ul>
         </div>
         <div>
-          <h3 className="font-bold mb-2">ABOUT US</h3>
-          <ul>
+          <h3
+            className="
+    font-bold 
+    text-left 
+    uppercase 
+    text-[14px] 
+    font-dm-sans 
+    text-[rgba(47,88,82,0.60)]
+    overflow-hidden 
+    text-ellipsis 
+    whitespace-nowrap
+    mb-[7px]
+  "
+            style={{
+              display: "-webkit-box",
+              transform: "rotate(-0.092deg)",
+              WebkitBoxOrient: "vertical",
+              WebkitLineClamp: 1,
+              lineHeight: "normal",
+            }}
+          >
+            ABOUT US
+          </h3>
+          <ul className="w-[200px] text-[#2F5852] text-[16px] font-dm-sans font-[16px] leading-normal">
             <li>Our Process</li>
             <li>Our Results</li>
             <li>Reviews</li>
@@ -64,46 +154,45 @@ export default function Footer() {
             height={40}
           />
           <div className="mt-6 border-t pt-6 text-center text-sm"></div>
-          <p className="mt-2">Terms and Conditions &bull; Privacy Policy</p>
-          <p className="mt-2">&copy; Foundation Learning</p>
+          <p className="mt-2 text-[#2F5852] text-[16px] font-dm-sans font-normal leading-normal">
+            Terms and Conditions &bull; Privacy Policy
+          </p>
+
+          <p className="mt-2 text-[rgba(47,88,82,0.60)] text-[16px] font-dm-sans font-normal leading-normal">
+            &copy; Foundation Learning
+          </p>
         </div>
 
         <div className="col-span-3 mt-6 pt-4 grid grid-cols-3 gap-4 text-left text-sm">
           <div className="border-r pr-4 pl-12 flex items-center space-x-2 mb-16">
-            <svg
-              className="w-8 h-8 text-orange-600 mb-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-            >
-              <path d="M3 2h18a1 1 0 011 1v18a1 1 0 01-1 1H3a1 1 0 01-1-1V3a1 1 0 011-1zm17 2H4v16h16V4zm-5 12a1 1 0 110 2H9a1 1 0 110-2h6zM8 6h8v2H8V6zm0 4h8v2H8v-2z" />
-            </svg>
+            <i className="fas fa-phone-alt fa-flip-horizontal text-orange-600 text-2xl mb-4"></i>
             <div>
-              <h3 className="font-bold">CALL</h3>
-              <p className="text-blue-600 font-semibold">(800) 511-6948</p>
+              <h3 className="font-weight-bold text-[14px] text-[rgba(47,88,82,0.60)] font-dm-sans font-normal uppercase leading-normal">
+                CALL
+              </h3>
+
+              <p className="text-[#2F5852] font-dm-sans text-[16px] font-bold leading-normal">
+                (800) 511-6948
+              </p>
             </div>
           </div>
 
           <div className="border-r px-4  items-start space-x-2">
             <div className="flex items-center space-x-2">
-              <svg
-                className="w-10 h-10 text-orange-600"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-              >
-                <path d="M12 2a10 10 0 00-8.5 15.3L2 22l4.7-1.5A10 10 0 1012 2zm0 18a8 8 0 110-16 8 8 0 010 16zm4.3-5.8c-.2-.1-1.5-.7-1.7-.8-.2-.1-.3-.1-.4.1-.1.2-.4.8-.5 1-.1.2-.2.2-.4.1-.2-.1-1-.4-1.9-1.2-.7-.6-1.2-1.4-1.3-1.6-.1-.2 0-.3.1-.4.1-.1.2-.2.3-.2.1 0 .2 0 .3.1.1.1.3.2.4.3.1.1.2.2.2.3.1.1.1.2 0 .3-.1.1-.2.3-.4.5-.2.2-.3.4-.5.5-.1.1-.1.2 0 .3.1.1.5.8 1.1 1.2.7.4 1.2.6 1.3.6.1 0 .2 0 .3-.1.1-.1.4-.5.5-.7.1-.2.1-.4 0-.5-.1-.1-.3-.2-.4-.2z" />
-              </svg>
-              <h3 className="font-bold">TEXT OR WHATSAPP</h3>
+              <i className="fas fa-comment-dots text-orange-600 text-3xl mr-2"></i>
+              <h3 className="font-weight-bold text-[14px] text-[rgba(47,88,82,0.60)] font-dm-sans font-normal uppercase leading-normal">
+                TEXT OR WHATSAPP
+              </h3>
             </div>
+
             <div className="flex flex-col">
-              <p className="text-blue-600 font-semibold pl-10">
+              <p className="text-[#2F5852] font-dm-sans text-[16px] font-bold leading-normal ml-10">
                 (800) 511-6948
               </p>
-              <p className="text-gray-600 mt-2 text-xs pl-10">
+              <p className="text-[rgba(47,88,82,0.60)] font-dm-sans text-[12px] font-normal leading-normal w-[291px] mt-2 pl-10">
                 By calling or texting (800) 511-6948, customers agree to receive
                 text messages. If you no longer wish to receive text messages,
-                you may opt-out at any time by replying &quot;STOP&quot;
+                you may opt out at any time by replying &quot;STOP&quot;.
               </p>
             </div>
           </div>
@@ -118,13 +207,17 @@ export default function Footer() {
               >
                 <path d="M12 2C7.03 2 3 6.03 3 11c0 5.97 9 11 9 11s9-5.03 9-11c0-4.97-4.03-9-9-9zm0 13c-2.2 0-4-1.8-4-4s1.8-4 4-4 4 1.8 4 4-1.8 4-4 4z" />
               </svg>
-              <h3 className="font-bold">ADDRESS</h3>
+              <h3 className="font-weight-bold text-[14px] text-[rgba(47,88,82,0.60)] font-dm-sans font-normal uppercase leading-normal">
+                ADDRESS
+              </h3>
             </div>
-            <div className="flex flex-col">
-              <p className="text-blue-600 font-semibold pl-12">
+            <div className="flex flex-col ml-12">
+              <p className="text-[#2F5852] font-dm-sans text-[16px] font-bold leading-normal">
                 80 Broad Street, New York City
               </p>
-              <p className="text-blue-600 font-semibold pl-12">10004 NY</p>
+              <p className="text-[#2F5852] font-dm-sans text-[16px] font-bold leading-normal">
+                10004 NY
+              </p>
             </div>
           </div>
         </div>
